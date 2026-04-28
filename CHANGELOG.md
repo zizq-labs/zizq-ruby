c5b2cf156bf98045a3ec6a2fc8c55c695acb3fbc
d11wtq <chris@w3style.co.uk>
2026-04-28 18:43:09 +1000

    Bump to 0.2.1.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 0.2.1
+
+
 ## 0.2.0
 
 - Implement `Client#count_jobs`
--- lib/zizq/version.rb
+++ lib/zizq/version.rb
@@ -5,5 +5,5 @@
 # frozen_string_literal: true
 
 module Zizq
-  VERSION = "0.2.0" #: String
+  VERSION = "0.2.1" #: String
 end
