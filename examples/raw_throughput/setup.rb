18583384f0151f6612851ab0c3067841e31fce75
d11wtq <chris@w3style.co.uk>
2026-06-01 08:08:08 +1000

    Fix setup.rb in raw_throughput Examples.
    I accidentally introduced Rail's specific '.presence` usage in the
    shared setup helpers here.

--- examples/raw_throughput/setup.rb
+++ examples/raw_throughput/setup.rb
@@ -11,7 +11,7 @@ require 'zizq'
 Zizq.configure do |c|
   c.url = ENV['ZIZQ_URL'] if ENV['ZIZQ_URL']
   c.format = ENV.fetch('ZIZQ_FORMAT', 'msgpack').to_sym
-  c.tls.ca = ENV['ZIZQ_CA'].presence
-  c.tls.client_cert = ENV['ZIZQ_CLIENT_CERT'].presence
-  c.tls.client_key = ENV['ZIZQ_CLIENT_KEY'].presence
+  c.tls.ca = ENV['ZIZQ_CA'].then{|s| s.to_s.empty? ? nil : s}
+  c.tls.client_cert = ENV['ZIZQ_CLIENT_CERT'].then{|s| s.to_s.empty? ? nil : s}
+  c.tls.client_key = ENV['ZIZQ_CLIENT_KEY'].then{|s| s.to_s.empty? ? nil : s}
 end
